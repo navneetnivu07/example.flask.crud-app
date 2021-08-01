dd458feeca8789503bb599256ef9b72a273dde44
Navaneeth Malingan <navneetnivu07@gmail.com>
2021-08-01 22:10:15 +0530

    db

--- bookmanager.py
+++ bookmanager.py
@@ -12,7 +12,7 @@ project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///bookdatabase.db' #database_file
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
